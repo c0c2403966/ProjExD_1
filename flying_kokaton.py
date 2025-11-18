a506ece1e06ff3ac2197eca2891c812ca324847e
Soma Takayanagi <c0c2403966@edu.teu.ac.jp>
2025-11-18 18:49:41 +0900

    名前変更

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -25,14 +25,16 @@ def main():
         screen.blit(bg_img, [-x+3200, 0]) #9
 
         key_lst = pg.key.get_pressed()
+        vx,vy=-1,0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            vy-=1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            vy+=1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            vx-=1    
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            vx+=1
+        kk_rct.move_ip(vx,vy)
         screen.blit(kk_img, kk_rct) #10
 
         pg.display.update()
